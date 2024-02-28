9a5971d0c0baca1419290f2726682561aa71026c
ErikAT04 <148959104+ErikAT04@users.noreply.github.com>
2024-02-28 20:07:09 +0100

    Añadido EJ3

---
+++ src/Ej1_4/Libro.java
@@ -0,0 +1,19 @@
+package Ej1_4;
+public class Libro extends Publicacion implements Prestable {
+    private boolean prestado=false;
+    public Libro(String isbn, String titulo, int anio) {
+        super(isbn, titulo, anio);
+    }
+    @Override
+    public void presta() {
+        this.prestado=true;
+    }
+    @Override
+    public void devuelve() {
+        this.prestado = true;
+    }
+    @Override
+    public boolean estaPrestado() {
+        return this.prestado;
+    }
+}
---
+++ src/Ej1_4/Publicacion.java
@@ -0,0 +1,19 @@
+package Ej1_4;
+
+public abstract class Publicacion {
+    protected String isbn;
+    protected String titulo;
+    protected int anio;
+    public Publicacion(String isbn, String titulo, int anio){
+        this.isbn=isbn;
+        this.titulo=titulo;
+        this.anio=anio;
+    }
+
+    @Override
+    public String toString() {
+        return ("ISBN: '" + isbn +"'\n"+
+                "Titulo: '"+ titulo + "'\n" +
+                "Año de salida: " + anio + ".");
+    }
+}
